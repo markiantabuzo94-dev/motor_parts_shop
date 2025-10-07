1d827e1f42ed3e96e13da6c970dc5f205342da9c
Markiany <markiantabuzo94@gmail.com>
2025-10-07 12:30:44 +0800

    Build Flutter web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "adb8a47fdff1fb70c609bcef5f845da6
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "83e29c16eed957dbf1c09cfcdeeaf15f",
+"flutter_bootstrap.js": "81f76a4eb18de5e38b8ac910a3cda7a9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
