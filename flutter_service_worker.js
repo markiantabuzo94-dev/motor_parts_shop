4ee3c8209584bd11f14143299ed0a49ed07c8bcc
Markiany <markiantabuzo94@gmail.com>
2025-10-11 10:49:13 +0800

    fix: hive init and web error

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,14 +34,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "adb8a47fdff1fb70c609bcef5f845da6
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "5bb401a1a6c7da98b216b8f202a5a788",
+"flutter_bootstrap.js": "8f5d62038e95f8bfb3ef664795a4e96e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "0d15a95a7b30e55267f671b2a6191908",
 "/": "0d15a95a7b30e55267f671b2a6191908",
-"main.dart.js": "4f8dfe5b2c895acb8fc053c2fb281fb4",
+"main.dart.js": "2b0476bc47c6469ef634687a14127396",
 "manifest.json": "dbae755b1b98b10e813bf30f5f8492c5",
 "version.json": "4a33a2f690da5ef61edd685d8cb70bdf"};
 // The application shell files that are downloaded before a service worker can
