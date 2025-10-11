7fd60de4577503385e126d95b5ec27cc424b8415
Markiany <markiantabuzo94@gmail.com>
2025-10-11 08:22:17 +0800

    Add build/web for GitHub Pages deploy

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "adb8a47fdff1fb70c609bcef5f845da6
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "3fc746ec4f0b8876b570f9f673a1b02e",
+"flutter_bootstrap.js": "3f9dbe30fcdf0b8d9ebfe58967d8a762",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
